2190dd8ce99be9f5e8cacbeb445df186c26089c3
Tiago Pires <tiago.pires@me.com>
2021-07-02 15:04:50 +1000

    Add dropdown component

--- src/App.js
+++ src/App.js
@@ -40,4 +40,28 @@ function Navbar(props) {
       </nav>
    );
 }
+
+function DropdownMenu() {
+   function DropdownItem(props) {
+      return (
+         <div>
+            <a href="#" className="menu-item" >
+               <span className="icon-button">{props.leftIcon}</span>
+               {props.children}
+               <span className="icon-right">{props.rightIcon}</span>
+            </a>
+         </div>
+      );
+   }
+   return (
+      <div className="dropdown">
+         <DropdownItem>My Profile</DropdownItem>
+         <DropdownItem 
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+         >
+         </DropdownItem>
+      </div>
+   )
+}
 export default App;
