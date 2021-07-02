b39ed15f03e8f4449a53e8a6c106d94e19fd24ed
Tiago Pires <tiago.pires@me.com>
2021-07-02 12:56:56 +1000

    Add correct icons and basic style for top icons @7:40

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
 import { ReactComponent as PlusIcon } from "./icons/plus.svg";
-import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
 
 function App() {
    return (
       <Navbar>
-         <NavItem icon={<MessengerIcon />} />
          <NavItem icon={<PlusIcon />} />
-         <NavItem icon={<CogIcon />} />
+         <NavItem icon={<BellIcon />} />
+         <NavItem icon={<MessengerIcon />} />
       </Navbar>
    );
 }
 
 function NavItem(props) {
    return (
-      <li className="nav-item"> {props.icon} </li>
+      <li className="nav-item">
+         <a href="#" className="icon-button">
+            {props.icon} 
+         </a>
+      </li>
    );
 }
 
